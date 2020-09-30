96479623cf2f187839f896d9494c084f714a8448
Florian Rau <bimac@users.noreply.github.com>
2020-09-30 18:48:46 +0200

    modify copyright notice

--- firmware/Bpod_stepper/Bpod_stepper.ino
+++ firmware/Bpod_stepper/Bpod_stepper.ino
@@ -1,3 +1,19 @@
+/*
+Bpod_stepper
+Copyright (C) 2020 Florian Rau
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed  WITHOUT ANY WARRANTY and without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "ArCOM.h"         // Import serial communication wrapper
 #include "SmoothStepper.h" // Import stepper library
 
--- firmware/Bpod_stepper/SmoothStepper.cpp
+++ firmware/Bpod_stepper/SmoothStepper.cpp
@@ -1,6 +1,18 @@
-// SmoothStepper.cpp
-//
-// Copyright (C) 2020 Florian Rau
+/*
+SmoothStepper
+Copyright (C) 2020 Florian Rau
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed  WITHOUT ANY WARRANTY and without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "Arduino.h"
 #include "SmoothStepper.h"
